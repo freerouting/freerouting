6cf794c584daf3b1056f898f94c819d4d48d4261
Andras Fuchs <andras.fuchs@gmail.com>
2021-10-13 23:20:38 +0200

    Update create-distribution-macos-signed.sh

--- distribution/create-distribution-macos-signed.sh
+++ distribution/create-distribution-macos-signed.sh
@@ -48,9 +48,9 @@ $JPKG_EXECUTABLE --input ../build/dist/ \
  --main-jar freerouting-executable.jar \
  --type $APP_TYPE --runtime-image .jdk14/runtime --app-version $APP_VERSION --license-file ../LICENSE 
 
-/usr/bin/codesign --force --sign $APPLE_DEVELOPER_ID --deep Freerouting-$APP_VERSION.app
+#/usr/bin/codesign --force --sign $APPLE_DEVELOPER_ID --deep Freerouting-$APP_VERSION.app
 
-/usr/bin/hdiutil create -srcfolder Freerouting-$APP_VERSION Freerouting-$APP_VERSION.dmg -fs HFS+
+#/usr/bin/hdiutil create -srcfolder Freerouting-$APP_VERSION Freerouting-$APP_VERSION.dmg -fs HFS+
 
-mv Freerouting-$APP_VERSION.dmg freerouting-$APP_VERSION-macos-x64.dmg
+#mv Freerouting-$APP_VERSION.dmg freerouting-$APP_VERSION-macos-x64.dmg
 
