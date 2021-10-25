94aa6a94a39f8bcc257063dc789ae07b5ca10022
Andras Fuchs <andras.fuchs@gmail.com>
2021-10-25 14:54:31 +0200

    Update create-distribution-jar.bat

--- distribution/create-distribution-jar.bat
+++ distribution/create-distribution-jar.bat
@@ -1,4 +1,4 @@
-@echo off
+@echo on
 
 SET  APP_VERSION=%1
 
@@ -6,3 +6,5 @@ set DIR="%~dp0\"
 cd %DIR%
 
 copy ..\build\dist\freerouting-executable.jar freerouting-%APP_VERSION%.jar
+
+echo "JAR created at \distribution\freerouting-%APP_VERSION%.jar"
