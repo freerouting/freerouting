a16ef01c26d68c2c054c6b7e16d743c2d18624f4
Andras Fuchs <andras.fuchs@gmail.com>
2022-04-28 20:38:27 +0200

    Update Linux x64 distribution script

--- distribution/create-distribution-linux-x64.sh
+++ distribution/create-distribution-linux-x64.sh
@@ -2,9 +2,8 @@
 
 export APP_VERSION=$1
 export APP_TYPE="app-image"
-#export JAVA_HOME="/usr/lib/jvm/java-11-openjdk-amd64/"
-export JPACKAGE_JVM="https://download.java.net/java/GA/jdk14/076bab302c7b4508975440c56f6cc26a/36/GPL/openjdk-14_linux-x64_bin.tar.gz"
-
+export JPACKAGE_JVM="https://github.com/adoptium/temurin17-binaries/releases/download/jdk-17.0.3+7/OpenJDK17U-jdk_x64_linux_hotspot_17.0.3_7.tar.gz"
+export JPACKAGE_HOME=.jdk/jdk-17.0.3+7
 
 SOURCE="${BASH_SOURCE[0]}"
 while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
@@ -17,16 +16,17 @@ DIR="$( cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )"
 cd $DIR
 
 
-if [ -d ".jdk14/jdk-14/" ]; then
-    echo "> jdk 14 for package generation already downloaded"
+if [ -d ".jdk/" ]; then
+    echo "> JDK for package generation already downloaded"
 else
-    mkdir -p .jdk14/ ; cd .jdk14
-    echo "> downloading jdk 14"
-    curl -o jdk14.tar.gz $JPACKAGE_JVM
-    echo "> unpacking jdk 14"
-    tar xvzf jdk14.tar.gz
+    mkdir -p .jdk/ ; cd .jdk
+    echo "> downloading JDK"
+    curl -o jdk.tar.gz $JPACKAGE_JVM
+    echo "> unpacking JDK"
+    tar xvzf jdk.tar.gz
+	cd ..
     echo "> creating runtime image"
-    $JAVA_HOME/bin/jlink -p "$JAVA_HOME/jmods" \
+    $JPACKAGE_HOME/bin/jlink -p "$JPACKAGE_HOME/jmods" \
         --add-modules java.desktop \
         --strip-debug \
         --no-header-files \
@@ -34,22 +34,21 @@ else
         --strip-native-commands \
         --vm=server \
         --compress=2 \
-        --output runtime
+        --output $JPACKAGE_HOME/runtime
 fi
 
 cd $DIR
 
-export JPKG_HOME=$(pwd)"/.jdk14/jdk-14/"
-export JPKG_EXECUTABLE=$JPKG_HOME/bin/jpackage
+export JPKG_HOME=$(pwd)"/"$JPACKAGE_HOME"/"
 
 cp ../LICENSE ../build/dist/LICENSE
 
-$JPKG_EXECUTABLE --input ../build/dist/ \
- --name Freerouting \
+$JPKG_HOME/bin/jpackage --input ../build/dist/ \
+ --name freerouting \
  --main-jar freerouting-executable.jar \
- --type $APP_TYPE --runtime-image .jdk14/runtime --app-version $APP_VERSION
+ --type $APP_TYPE --runtime-image $JPACKAGE_HOME/runtime --app-version $APP_VERSION
  
-mv Freerouting freerouting-$APP_VERSION-linux-x64
+mv freerouting freerouting-$APP_VERSION-linux-x64
 cp ../build/dist/LICENSE freerouting-$APP_VERSION-linux-x64/LICENSE
 mv ../build/dist/freerouting-executable.jar freerouting-$APP_VERSION-executable.jar 
  
