326db2007250902d20627502095d08cc6d8d1c29
Andras Fuchs <andras.fuchs@gmail.com>
2022-04-28 10:49:46 +0200

    Fix for x64 and x86 Windows installer creation batch files

--- distribution/create-distribution-windows-x86.bat
+++ distribution/create-distribution-windows-x86.bat
@@ -1,8 +1,9 @@
 @echo off
 
-SET  APP_VERSION=%1
-SET  APP_TYPE="msi"
-SET  JPACKAGE_JVM="https://github.com/AdoptOpenJDK/openjdk14-binaries/releases/download/jdk-14+36/OpenJDK14U-jdk_x86-32_windows_hotspot_14_36.zip"
+SET APP_VERSION=%1
+SET APP_TYPE="msi"
+SET JPACKAGE_JVM="https://github.com/adoptium/temurin17-binaries/releases/download/jdk-17.0.2+8/OpenJDK17U-jdk_x86-32_windows_hotspot_17.0.2_8.zip"
+SET JPACKAGE_HOME=.jdk\jdk-17.0.2+8
 
 set DIR="%~dp0\"
 cd %DIR%
@@ -12,30 +13,33 @@ for %%X in (curl.exe) do (set FOUNDCURL=%%~$PATH:X)
 
 if not defined FOUND7Z (
   echo "ERROR: please make sure that 7Zip is installed and on the path."
+  goto exit
 )
 
 if not defined FOUNDCURL (
   echo "ERROR: please make sure that Curl is installed and on the path."
+  goto exit
 )
 
-if exist ".jdk14\jdk-14+36\" (
-    echo "> jdk 14 for package generation already downloaded"
+if exist ".jdk\" (
+    echo "> JDK for package generation already downloaded"
 ) else (
-    mkdir .jdk14\
-    cd .jdk14
-    echo "> downloading jdk 14 x86"
-    curl -L -o jdk14_x86.zip %JPACKAGE_JVM%
-    echo "> unpacking jdk 14 x86"
-    7z x jdk14_x86.zip
+    mkdir .jdk\
+    cd .jdk
+    echo "> downloading JDK"
+    curl -L -o jdk.zip %JPACKAGE_JVM%
+    echo "> unpacking JDK"
+    7z x jdk.zip
+	cd ..
     echo "> creating runtime image"
-    %JAVA_HOME%\bin\jlink -p "%JAVA_HOME%\jmods" --add-modules java.desktop --strip-debug --no-header-files --no-man-pages --strip-native-commands --vm=server --compress=2 --output runtime
+    "%JPACKAGE_HOME%\bin\jlink.exe" -p "%JPACKAGE_HOME%\jmods" --add-modules java.desktop --strip-debug --no-header-files --no-man-pages --strip-native-commands --vm=server --compress=2 --output "%JPACKAGE_HOME%\runtime"
 )
 
 cd %DIR%
 
-set JPKG_HOME=.jdk14\jdk-14+36\
-set JPKG_EXECUTABLE=%JPKG_HOME%\bin\jpackage
-
-%JPKG_EXECUTABLE% --input ..\build\dist\ --name Freerouting --main-jar freerouting-executable.jar --type %APP_TYPE% --runtime-image .jdk14\runtime --app-version %APP_VERSION% --win-per-user-install --win-menu --win-menu-group Freerouting --license-file ..\LICENSE 
+echo "> creating installer .msi"
+"%JPACKAGE_HOME%\bin\jpackage.exe" --input "..\build\libs" --main-jar "freerouting-executable.jar" --name "Freerouting" --type %APP_TYPE% --runtime-image "%JPACKAGE_HOME%\runtime" --app-version %APP_VERSION% --win-per-user-install --win-menu --win-menu-group Freerouting --license-file "..\LICENSE"
 
 move Freerouting-%APP_VERSION%.msi freerouting-%APP_VERSION%-windows-x86.msi
+
+:exit
