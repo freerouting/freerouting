8f8dbf572b5a288821ae42f8cc7b4ecc8bcddbcb
Andras Fuchs <andras.fuchs@gmail.com>
2022-04-29 15:58:47 +0200

    Add the runtime builder step

--- distribution/create-distribution-linux-x64.sh
+++ distribution/create-distribution-linux-x64.sh
@@ -17,11 +17,24 @@ cd $DIR
 
 cp ../LICENSE ../build/dist/LICENSE
 
+echo "> Building the Java runtime"
+$JAVA_HOME/bin/jlink -p "$JAVA_HOME/jmods" \
+        --add-modules java.desktop \
+        --strip-debug \
+        --no-header-files \
+        --no-man-pages \
+        --strip-native-commands \
+        --vm=server \
+        --compress=2 \
+        --output $JAVA_HOME/runtime
+
+echo "> Creating the package"
 $JAVA_HOME/bin/jpackage --input ../build/dist/ \
  --name freerouting \
  --main-jar freerouting-executable.jar \
  --type $APP_TYPE --runtime-image $JAVA_HOME/runtime --app-version $APP_VERSION
- 
+
+echo "> Composing the distribution file"
 mv freerouting freerouting-$APP_VERSION-linux-x64
 cp ../build/dist/LICENSE freerouting-$APP_VERSION-linux-x64/LICENSE
 mv ../build/dist/freerouting-executable.jar freerouting-$APP_VERSION-executable.jar 
