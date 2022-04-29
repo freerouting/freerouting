1c8a3f9d8f6f593f1fc6f8e0006f7072883f9373
Andras Fuchs <andras.fuchs@gmail.com>
2022-04-29 16:08:58 +0200

    Remove the symlink follower routine

--- distribution/create-distribution-linux-x64.sh
+++ distribution/create-distribution-linux-x64.sh
@@ -5,16 +5,6 @@ echo "> JAVA_HOME="$JAVA_HOME
 export APP_VERSION=$1
 export APP_TYPE="app-image"
 
-SOURCE="${BASH_SOURCE[0]}"
-while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
-  DIR="$( cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )"
-  SOURCE="$(readlink "$SOURCE")"
-  [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE" # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
-done
-DIR="$( cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )"
-
-cd $DIR
-
 cp ../LICENSE ../build/dist/LICENSE
 
 echo "> Building the Java runtime"
