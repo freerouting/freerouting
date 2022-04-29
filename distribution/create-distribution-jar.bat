28178f9d8300b0315e357426ab7ed57600e51896
Andras Fuchs <andras.fuchs@gmail.com>
2022-04-29 16:33:24 +0200

    Change the dist folder from libs to dist

--- distribution/create-distribution-jar.bat
+++ distribution/create-distribution-jar.bat
@@ -2,9 +2,12 @@
 
 SET  APP_VERSION=%1
 
+echo "> JAVA_HOME="%JAVA_HOME%
+
 set DIR="%~dp0\"
+echo "> Distribution directory="%DIR%
 cd %DIR%
 
-copy ..\build\libs\freerouting-executable.jar freerouting-%APP_VERSION%.jar
+copy ..\build\dist\freerouting-executable.jar freerouting-%APP_VERSION%.jar
 
 echo "JAR created at \distribution\freerouting-%APP_VERSION%.jar"
