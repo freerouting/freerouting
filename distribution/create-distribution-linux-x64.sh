4d47e97760486823f98be581f069381c5cfbbc6a
Andras Fuchs <andras.fuchs@gmail.com>
2022-04-29 16:33:37 +0200

    Fix a typo

--- distribution/create-distribution-linux-x64.sh
+++ distribution/create-distribution-linux-x64.sh
@@ -13,7 +13,7 @@ while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symli
 done
 DIR="$( cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )"
 
-echo "> Project directory="$DIR
+echo "> Distribution directory="$DIR
 cd $DIR
 
 cp ../LICENSE ../build/dist/LICENSE
