39952ceebfe91b4a481bc07a8f7934b2d706c7d7
Andras Fuchs <andras.fuchs@gmail.com>
2022-04-29 15:47:15 +0200

    Cache Gradle with Java

--- distribution/create-distribution-linux-x64.sh
+++ distribution/create-distribution-linux-x64.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+echo "> JAVA_HOME="$JAVA_HOME
+
 export APP_VERSION=$1
 export APP_TYPE="app-image"
 export JPACKAGE_JVM="https://github.com/adoptium/temurin17-binaries/releases/download/jdk-17.0.3+7/OpenJDK17U-jdk_x64_linux_hotspot_17.0.3_7.tar.gz"
