d45af3d135243075cee29856c3d3169ac9430ff2
Andras Fuchs <andras.fuchs@gmail.com>
2022-05-11 17:35:47 +0200

    Update identifier string handling in .SES files

--- src/main/java/app/freerouting/datastructures/IdentifierType.java
+++ src/main/java/app/freerouting/datastructures/IdentifierType.java
@@ -13,36 +13,36 @@ public class IdentifierType
      * Defines the reserved characters and the string for quoting identifiers containing
      * reserved characters for a new instance of Identifier.
      */
-    public IdentifierType(String [] p_reserved_chars, String p_string_quote)
+    public IdentifierType(String[] p_reserved_chars, String p_string_quote)
     {
         reserved_chars = p_reserved_chars;
         string_quote = p_string_quote;
     }
     
     /**
-     * Writes p_name after puttiong it into quotes, if it contains reserved characters or blanks.
+     * Writes p_name after putting it into quotes, if it contains reserved characters or blanks.
      */
     public void write(String p_name, OutputStreamWriter p_file)
     {
+        // remove the double quotes from the identifiers
+        while ((p_name.length() > 2) && (p_name.charAt(0) == '"') && (p_name.charAt(p_name.length() - 1) == '"'))
+        {
+            p_name = p_name.substring(1, p_name.length() - 2);
+        }
+
         try
         {
-            if (is_legal(p_name))
-            {
-                p_file.write(p_name);
-            }
-            else
-            {
-                p_file.write(quote(p_name));
-            }
+            // always put quotes around the identifiers even if they don't have illegal characters
+            p_file.write(quote(p_name));
         }
         catch (java.io.IOException e)
         {
-            FRLogger.warn("IndentFileWriter.new_line: unable to write to file");
+            FRLogger.warn("IdentifierType.write: unable to write to file");
         }
     }
     
     /**
-     * Looks, if p_string dous not contain reserved characters or blanks.
+     * Looks, if p_string does not contain reserved characters or blanks.
      */
     private boolean is_legal( String p_string)
     {
