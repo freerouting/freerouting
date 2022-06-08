194aa9583287224bafe092164ea0b1a7ff9c78a2
Leandro Heck <leoheck@gmail.com>
2022-06-08 10:28:31 -0300

    Improve Display > Colors, window sizes (on Ubuntu)

--- src/main/java/app/freerouting/gui/ColorManager.java
+++ src/main/java/app/freerouting/gui/ColorManager.java
@@ -28,35 +28,36 @@ import app.freerouting.boardgraphics.GraphicsContext;
  */
 public class ColorManager extends BoardSavableSubWindow
 {
-    
+
     /** Creates a new instance of ColorManager */
     public ColorManager(BoardFrame p_board_frame)
     {
         GraphicsContext graphics_context = p_board_frame.board_panel.board_handling.graphics_context;
-        java.util.ResourceBundle resources = 
+        java.util.ResourceBundle resources =
                 java.util.ResourceBundle.getBundle("app.freerouting.gui.Default", p_board_frame.get_locale());
         this.setTitle(resources.getString("color_manager"));
         final JPanel panel =  new JPanel();
-        final int textfield_height = 17;
+        final int textfield_height = 20;
         final int table_width = 1100;
         final int item_color_table_height = graphics_context.item_color_table.getRowCount() * textfield_height;
-        panel.setPreferredSize(new Dimension(10 + table_width, 70 + item_color_table_height));
-        
+
+        panel.setPreferredSize(new Dimension(10 + table_width, 90 + item_color_table_height));
+
         this.item_color_table = new JTable(graphics_context.item_color_table);
         item_color_table.setPreferredScrollableViewportSize(new Dimension(table_width, item_color_table_height));
         JScrollPane item_scroll_pane = init_color_table(item_color_table, p_board_frame.get_locale());
         panel.add(item_scroll_pane, BorderLayout.NORTH);
-        
+
         this.other_color_table = new JTable(graphics_context.other_color_table);
         this.other_color_table.setPreferredScrollableViewportSize(new Dimension(table_width, textfield_height));
         JScrollPane other_scroll_pane = init_color_table(other_color_table, p_board_frame.get_locale());
-        panel.add(other_scroll_pane, BorderLayout.SOUTH);       
+        panel.add(other_scroll_pane, BorderLayout.SOUTH);
         getContentPane().add(panel, BorderLayout.CENTER);
         p_board_frame.set_context_sensitive_help(this, "WindowDisplay_Colors");
         this.pack();
         this.setResizable(false);
     }
-    
+
     /**
      * Reassigns the table model variables because they may have changed in p_graphics_context.
      */
@@ -65,9 +66,9 @@ public class ColorManager extends BoardSavableSubWindow
         this.item_color_table.setModel(p_graphics_context.item_color_table);
         this.other_color_table.setModel(p_graphics_context.other_color_table);
     }
-    
-    
-    
+
+
+
     /**
      * Initializes p_color_table and return the created scroll_pane of the color table.
      */
@@ -77,11 +78,11 @@ public class ColorManager extends BoardSavableSubWindow
         JScrollPane scroll_pane = new JScrollPane(p_color_table);
         //Set up renderer and editor for the Color columns.
         p_color_table.setDefaultRenderer(Color.class, new ColorRenderer(true));
-        
+
         setUpColorEditor(p_color_table, p_locale);
         return scroll_pane;
     }
-    
+
     //Set up the editor for the Color cells.
     private static void setUpColorEditor(JTable p_table, java.util.Locale p_locale)
     {
@@ -96,12 +97,12 @@ public class ColorManager extends BoardSavableSubWindow
         button.setBackground(Color.white);
         button.setBorderPainted(false);
         button.setMargin(new Insets(0,0,0,0));
-        
+
         //Now create an editor to encapsulate the button, and
         //set it up as the editor for all Color cells.
         final ColorEditor colorEditor = new ColorEditor(button);
         p_table.setDefaultEditor(Color.class, colorEditor);
-        
+
         //Set up the dialog that the button brings up.
         final JColorChooser colorChooser = new JColorChooser();
         ActionListener okListener = new ActionListener()
@@ -111,11 +112,11 @@ public class ColorManager extends BoardSavableSubWindow
                 colorEditor.currentColor = colorChooser.getColor();
             }
         };
-        java.util.ResourceBundle resources = 
+        java.util.ResourceBundle resources =
                 java.util.ResourceBundle.getBundle("app.freerouting.gui.Default", p_locale);
         final JDialog dialog = JColorChooser.createDialog(button,
                 resources.getString("pick_a_color"), true, colorChooser, okListener, null);
-        
+
         //Here's the code that brings up the dialog.
         button.addActionListener(new ActionListener()
         {
@@ -130,23 +131,23 @@ public class ColorManager extends BoardSavableSubWindow
             }
         });
     }
-    
+
     private final JTable item_color_table;
     private final JTable other_color_table;
-    
+
     private static class ColorRenderer extends JLabel implements TableCellRenderer
     {
         Border unselectedBorder = null;
         Border selectedBorder = null;
         boolean isBordered = true;
-        
+
         public ColorRenderer(boolean p_is_bordered)
         {
             super();
             this.isBordered = p_is_bordered;
             setOpaque(true); //MUST do this for background to show up.
         }
-        
+
         public Component getTableCellRendererComponent(
                 JTable p_table, Object p_color,
                 boolean p_is_selected, boolean p_has_focus,
@@ -178,7 +179,7 @@ public class ColorManager extends BoardSavableSubWindow
             return this;
         }
     }
-    
+
     /**
      * The editor button that brings up the dialog.
      * We extend DefaultCellEditor for convenience,
@@ -190,7 +191,7 @@ public class ColorManager extends BoardSavableSubWindow
     private static class ColorEditor extends DefaultCellEditor
     {
         Color currentColor = null;
-        
+
         public ColorEditor(JButton b)
         {
             super(new JCheckBox()); //Unfortunately, the constructor
@@ -198,7 +199,7 @@ public class ColorManager extends BoardSavableSubWindow
             //or text field.
             editorComponent = b;
             setClickCountToStart(1); //This is usually 1 or 2.
-            
+
             //Must do this so that editing stops when appropriate.
             b.addActionListener(new ActionListener()
             {
@@ -208,17 +209,17 @@ public class ColorManager extends BoardSavableSubWindow
                 }
             });
         }
-        
+
         protected void fireEditingStopped()
         {
             super.fireEditingStopped();
         }
-        
+
         public Object getCellEditorValue()
         {
             return currentColor;
         }
-        
+
         public Component getTableCellEditorComponent(JTable table,
                 Object value,
                 boolean isSelected,
