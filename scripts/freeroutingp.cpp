cd603d44543e5fd84cbd4ad5345770fa3815b2c6
L1uTongwei <1347277058@qq.com>
2024-06-22 16:14:45 +0800

    freerouting progress & daemon hacked

--- scripts/freeroutingp.cpp
+++ scripts/freeroutingp.cpp
@@ -14,36 +14,36 @@ void split(std::string str, const char split){
 int main(){
     int times = 0, clocks = 0;
     std::string line;
-    while(true){
-        std::getline(std::cin, line);
+    while(std::getline(std::cin, line)){
         split(line, ' ');
-        if(splitArray.size() == 7 && splitArray[2] == "[Thread-0]" 
-            && splitArray[3] == "INFO" && splitArray[4] == "Starting"
-            && splitArray[5] == "auto-routing..."){
+        // for(auto it = splitArray.begin(); it != splitArray.end(); it++){
+        //     std::clog << '(' << it - splitArray.begin() << " \"" << *it << "\"" << ") ";
+        // }
+        // std::clog << std::endl;
+        if(splitArray[3] == "INFO" && splitArray[5] == "Starting"
+            && splitArray[6] == "auto-routing..."){
             clocks = clock();
             fprintf(stderr, "{\"status\": \"startRoute\"}\n");
-        }else if(splitArray.size() == 12 && splitArray[2] == "[Thread-0]" 
-            && splitArray[3] == "INFO" && splitArray[4] == "Auto-routing"
-            && splitArray[5] == "was" && splitArray[6] == "completed"){
-            fprintf(stderr, "{\"status\": \"routingResult\", \"times\": %d}\n", times);
-        }else if(splitArray.size() == 10 && splitArray[2] == "[Thread-0]" 
-            && splitArray[3] == "INFO" && splitArray[4] == "Starting"
-            && splitArray[5] == "route" && splitArray[6] == "optimization"){
+        }else if(splitArray[3] == "INFO" && splitArray[5] == "Auto-routing"
+            && splitArray[6] == "was" && splitArray[7] == "completed"){
+            fprintf(stderr, "{\"status\": \"routingResult\"}");
+        }else if(splitArray[3] == "INFO" && splitArray[5] == "Starting"
+            && splitArray[6] == "route" && splitArray[7] == "optimization"){
             fprintf(stderr, "{\"status\": \"startOptimize\"}\n");
-        }else if(splitArray.size() == 11 && splitArray[2] == "[Thread-0]"
-            && splitArray[3] == "INFO" && splitArray[4] == "Route"
-            && splitArray[5] == "optimization" && splitArray[6] == "was"
-            && splitArray[7] == "completed"){
+        }else if(splitArray[3] == "INFO" && splitArray[5] == "Route"
+            && splitArray[6] == "optimization" && splitArray[7] == "was"
+            && splitArray[8] == "completed"){
             fprintf(stderr, "{\"status\": \"optimizeResult\"}\n");
         }
-        if(splitArray.size() == 6 && splitArray[2] == "[Thread-0]" 
-            && splitArray[3] == "INFO" && splitArray[4] == "Saving"){
+        if(splitArray[3] == "INFO" && splitArray[5] == "Saving"){
             times++;
             printf("%s %s [Thread-0] INFO Auto-routing progress #%d completed in %lf seconds.",
                 splitArray[0].c_str(), splitArray[1].c_str(), times, (clock() - clocks) / (double)CLOCKS_PER_SEC);
             clocks = clock();
-            fprintf(stderr, "{\"status\": \"routingProgress\", \"times\": %d}\n", times);
-        }else puts(line.c_str());
+            fprintf(stderr, "{\"status\": \"routingProgress\"}");
+        }
+        puts(line.c_str());
+        splitArray.clear();
     }
     return 0;
 }
