8b6e9bb03484cc9cdea1bead435b3ca05004c886
Andras Fuchs <andras.fuchs@gmail.com>
2024-09-23 16:16:41 +0200

    New endpoint for session creation

--- src/main/java/app/freerouting/api/v1/SessionControllerV1.java
+++ src/main/java/app/freerouting/api/v1/SessionControllerV1.java
@@ -2,12 +2,11 @@ package app.freerouting.api.v1;
 
 import app.freerouting.core.Session;
 import app.freerouting.management.SessionManager;
-import jakarta.ws.rs.GET;
-import jakarta.ws.rs.Path;
-import jakarta.ws.rs.PathParam;
-import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.*;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
+
+import java.util.UUID;
 
 import static app.freerouting.management.gson.GsonProvider.GSON;
 
@@ -25,6 +24,23 @@ public class SessionControllerV1
   {
     // TODO: filter the list of sessions to only include the ones that the user has access to
     return Response.ok(GSON.toJson(SessionManager.getInstance().listSessionIds())).build();
+  }
+
+  @POST
+  @Path("/create")
+  @Produces(MediaType.APPLICATION_JSON)
+  public Response createSession()
+  {
+    // TODO: create a new session using the authenticated user as the owner
+    Session newSession = SessionManager.getInstance().createSession(UUID.randomUUID());
+    if (newSession == null)
+    {
+      return Response.status(Response.Status.INTERNAL_SERVER_ERROR).entity("{}").build();
+    }
+    else
+    {
+      return Response.ok(GSON.toJson(newSession)).build();
+    }
   }
 
   @GET
