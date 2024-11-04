6dff4178f28e38d50d54a7ed6dcfe6214a88a1e4
Andras Fuchs <andras.fuchs@gmail.com>
2024-11-04 16:33:40 +0100

    Handle invalid endpoint calls and internal server errors in a custom way to help API users

--- src/main/java/app/freerouting/api/NotFoundExceptionMapper.java
+++ src/main/java/app/freerouting/api/NotFoundExceptionMapper.java
@@ -1,6 +1,7 @@
 package app.freerouting.api;
 
 import com.google.gson.JsonObject;
+import jakarta.ws.rs.NotFoundException;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
@@ -10,18 +11,18 @@ import jakarta.ws.rs.ext.Provider;
 import static app.freerouting.management.gson.GsonProvider.GSON;
 
 @Provider
-public class ApiExceptionMapper implements ExceptionMapper<Throwable>
+public class NotFoundExceptionMapper implements ExceptionMapper<NotFoundException>
 {
   @Override
   @Produces(MediaType.APPLICATION_JSON)
-  public Response toResponse(Throwable exception)
+  public Response toResponse(NotFoundException exception)
   {
     JsonObject errorMessage = new JsonObject();
-    errorMessage.addProperty("error", exception.getMessage());
+    errorMessage.addProperty("error", "The requested resource was not found.");
     errorMessage.addProperty("documentation", "https://github.com/freerouting/freerouting/blob/master/docs/API_v1.md");
 
     String prettyErrorMessage = GSON.toJson(errorMessage);
 
-    return Response.status(Response.Status.INTERNAL_SERVER_ERROR).entity(prettyErrorMessage).build();
+    return Response.status(Response.Status.NOT_FOUND).entity(prettyErrorMessage).build();
   }
 }
