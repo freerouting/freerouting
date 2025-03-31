361d4854d9c97a480fe6bbd73e550afe7217c805
Andras Fuchs <andras.fuchs@gmail.com>
2025-03-31 20:22:22 +0200

    Add two unit tests

---
+++ src/test/java/app/freerouting/tests/Issue159Test.java
@@ -0,0 +1,21 @@
+package app.freerouting.tests;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class Issue159Test extends TestBasedOnAnIssue
+{
+  @Test
+  void test_Issue_159_Out_of_memory_error()
+  {
+    var job = GetRoutingJob("Issue159-setonix_2hp-pcb.dsn");
+
+    job = RunRoutingJob(job, job.routerSettings);
+
+    var statsAfter = GetBoardStatistics(job);
+
+    assertEquals(0, statsAfter.connections.incompleteCount, "The incomplete count should be 0");
+    assertEquals(0, statsAfter.clearanceViolations.totalCount, "The total count of clearance violations should be 0");
+  }
+}
---
+++ src/test/java/app/freerouting/tests/Issue229Test.java
@@ -0,0 +1,21 @@
+package app.freerouting.tests;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class Issue229Test extends TestBasedOnAnIssue
+{
+  @Test
+  void test_Issue_229_Keepout_zone_was_not_exported_correctly()
+  {
+    var job = GetRoutingJob("Issue229-display-8-digit-hc595.dsn");
+
+    job = RunRoutingJob(job, job.routerSettings);
+
+    var statsAfter = GetBoardStatistics(job);
+
+    assertEquals(0, statsAfter.connections.incompleteCount, "The incomplete count should be 0");
+    assertEquals(0, statsAfter.clearanceViolations.totalCount, "The total count of clearance violations should be 0");
+  }
+}
