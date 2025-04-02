9f67492e472137b5fcf66e346b74644ba5d5841b
Andras Fuchs <andras.fuchs@gmail.com>
2025-04-02 16:41:56 +0200

    Simplify the router batch loop

--- src/main/java/app/freerouting/autoroute/BatchAutorouter.java
+++ src/main/java/app/freerouting/autoroute/BatchAutorouter.java
@@ -97,7 +97,7 @@ public class BatchAutorouter extends NamedAlgorithm
       {
         router_instance.is_interrupted = true;
       }
-      still_unrouted_items = router_instance.autoroute_pass(curr_pass_no);
+      still_unrouted_items = autoroute_pass(curr_pass_no);
       if (still_unrouted_items && !router_instance.is_interrupted && updated_routing_board == null)
       {
         routerSettings.increment_pass_no();
@@ -110,6 +110,163 @@ public class BatchAutorouter extends NamedAlgorithm
       --curr_pass_no;
     }
     return curr_pass_no;
+  }
+
+  /**
+   * Auto-routes one ripup pass of all items of the board. Returns false, if the board is already
+   * completely routed.
+   */
+  private boolean autoroute_pass(int p_pass_no)
+  {
+    try
+    {
+      LinkedList<Item> autoroute_item_list = new LinkedList<>();
+      Set<Item> handled_items = new TreeSet<>();
+      Iterator<UndoableObjects.UndoableObjectNode> it = board.item_list.start_read_object();
+      for (; ; )
+      {
+        UndoableObjects.Storable curr_ob = board.item_list.read_object(it);
+        if (curr_ob == null)
+        {
+          break;
+        }
+        if (curr_ob instanceof Connectable && curr_ob instanceof Item curr_item)
+        {
+          // This is a connectable item, like PolylineTrace or Pin
+          if (!curr_item.is_routable())
+          {
+            if (!handled_items.contains(curr_item))
+            {
+
+              // Let's go through all nets of this item
+              for (int i = 0; i < curr_item.net_count(); ++i)
+              {
+                int curr_net_no = curr_item.get_net_no(i);
+                Set<Item> connected_set = curr_item.get_connected_set(curr_net_no);
+                for (Item curr_connected_item : connected_set)
+                {
+                  if (curr_connected_item.net_count() <= 1)
+                  {
+                    handled_items.add(curr_connected_item);
+                  }
+                }
+                int net_item_count = board.connectable_item_count(curr_net_no);
+
+                // If the item is not connected to all other items of the net, we add it to the auto-router's to-do list
+                if ((connected_set.size() < net_item_count) && (!curr_item.has_ignored_nets()))
+                {
+                  autoroute_item_list.add(curr_item);
+                }
+              }
+            }
+          }
+        }
+      }
+
+      // If there are no items to route, we're done
+      if (autoroute_item_list.isEmpty())
+      {
+        this.air_line = null;
+        return false;
+      }
+
+      int items_to_go_count = autoroute_item_list.size();
+      int ripped_item_count = 0;
+      int not_routed = 0;
+      int routed = 0;
+      int skipped = 0;
+      BoardStatistics stats = board.get_statistics();
+      RouterCounters routerCounters = new RouterCounters();
+      routerCounters.passCount = p_pass_no;
+      routerCounters.queuedToBeRoutedCount = items_to_go_count;
+      routerCounters.skippedCount = skipped;
+      routerCounters.rippedCount = ripped_item_count;
+      routerCounters.failedToBeRoutedCount = not_routed;
+      routerCounters.routedCount = routed;
+      routerCounters.incompleteCount = new RatsNest(board).incomplete_count();
+
+      this.fireBoardUpdatedEvent(stats, routerCounters, this.board);
+
+      // TODO: Start mutliple instances of the following part in parallel, wait for the results and keep the best one
+
+      // Shuffle the items to route
+      shuffle(autoroute_item_list, new Random());
+
+      // Let's go through all items to route
+      for (Item curr_item : autoroute_item_list)
+      {
+        // If the user requested to stop the auto-router, we stop it
+        if (this.is_interrupted)
+        {
+          break;
+        }
+
+        // Let's go through all nets of this item
+        for (int i = 0; i < curr_item.net_count(); ++i)
+        {
+          // If the user requested to stop the auto-router, we stop it
+          if (this.thread.is_stop_auto_router_requested())
+          {
+            this.is_interrupted = true;
+            break;
+          }
+
+          // We visually mark the area of the board, which is changed by the auto-router
+          board.start_marking_changed_area();
+
+          // Do the auto-routing step for this item (typically PolylineTrace or Pin)
+          SortedSet<Item> ripped_item_list = new TreeSet<>();
+          //boolean useSlowAlgorithm = this.board.rules.get_use_slow_autoroute_algorithm();
+          boolean useSlowAlgorithm = p_pass_no % 4 == 0;
+          var autorouterResult = autoroute_item(curr_item, curr_item.get_net_no(i), ripped_item_list, p_pass_no, useSlowAlgorithm);
+          if (autorouterResult.state == AutorouteAttemptState.ROUTED)
+          {
+            // The item was successfully routed
+            ++routed;
+          }
+          else if ((autorouterResult.state == AutorouteAttemptState.ALREADY_CONNECTED) || (autorouterResult.state == AutorouteAttemptState.NO_UNCONNECTED_NETS) || (autorouterResult.state == AutorouteAttemptState.CONNECTED_TO_PLANE))
+          {
+            // The item doesn't need to be routed
+            ++skipped;
+          }
+          else
+          {
+            job.logDebug("Autorouter " + autorouterResult.details);
+            ++not_routed;
+          }
+          --items_to_go_count;
+          ripped_item_count += ripped_item_list.size();
+
+          BoardStatistics boardStatistics = board.get_statistics();
+          routerCounters.passCount = p_pass_no;
+          routerCounters.queuedToBeRoutedCount = items_to_go_count;
+          routerCounters.skippedCount = skipped;
+          routerCounters.rippedCount = ripped_item_count;
+          routerCounters.failedToBeRoutedCount = not_routed;
+          routerCounters.routedCount = routed;
+          routerCounters.incompleteCount = new RatsNest(board).incomplete_count();
+          this.fireBoardUpdatedEvent(boardStatistics, routerCounters, this.board);
+        }
+      }
+
+      if (this.remove_unconnected_vias)
+      {
+        remove_tails(Item.StopConnectionOption.NONE);
+      }
+      else
+      {
+        remove_tails(Item.StopConnectionOption.FANOUT_VIA);
+      }
+
+      // We are done with this pass
+      this.air_line = null;
+      return true;
+    } catch (Exception e)
+    {
+      job.logError("Something went wrong during the auto-routing", e);
+      this.air_line = null;
+      return false;
+    }
   }
 
   @Override
@@ -256,161 +413,6 @@ public class BatchAutorouter extends NamedAlgorithm
     }
 
     return !this.is_interrupted;
-  }
-
-  /**
-   * Auto-routes one ripup pass of all items of the board. Returns false, if the board is already
-   * completely routed.
-   */
-  private boolean autoroute_pass(int p_pass_no)
-  {
-    try
-    {
-      LinkedList<Item> autoroute_item_list = new LinkedList<>();
-      Set<Item> handled_items = new TreeSet<>();
-      Iterator<UndoableObjects.UndoableObjectNode> it = board.item_list.start_read_object();
-      for (; ; )
-      {
-        UndoableObjects.Storable curr_ob = board.item_list.read_object(it);
-        if (curr_ob == null)
-        {
-          break;
-        }
-        if (curr_ob instanceof Connectable && curr_ob instanceof Item curr_item)
-        {
-          // This is a connectable item, like PolylineTrace or Pin
-          if (!curr_item.is_routable())
-          {
-            if (!handled_items.contains(curr_item))
-            {
-
-              // Let's go through all nets of this item
-              for (int i = 0; i < curr_item.net_count(); ++i)
-              {
-                int curr_net_no = curr_item.get_net_no(i);
-                Set<Item> connected_set = curr_item.get_connected_set(curr_net_no);
-                for (Item curr_connected_item : connected_set)
-                {
-                  if (curr_connected_item.net_count() <= 1)
-                  {
-                    handled_items.add(curr_connected_item);
-                  }
-                }
-                int net_item_count = board.connectable_item_count(curr_net_no);
-
-                // If the item is not connected to all other items of the net, we add it to the auto-router's to-do list
-                if ((connected_set.size() < net_item_count) && (!curr_item.has_ignored_nets()))
-                {
-                  autoroute_item_list.add(curr_item);
-                }
-              }
-            }
-          }
-        }
-      }
-
-      // If there are no items to route, we're done
-      if (autoroute_item_list.isEmpty())
-      {
-        this.air_line = null;
-        return false;
-      }
-
-      int items_to_go_count = autoroute_item_list.size();
-      int ripped_item_count = 0;
-      int not_routed = 0;
-      int routed = 0;
-      int skipped = 0;
-      BoardStatistics stats = board.get_statistics();
-      RouterCounters routerCounters = new RouterCounters();
-      routerCounters.passCount = p_pass_no;
-      routerCounters.queuedToBeRoutedCount = items_to_go_count;
-      routerCounters.skippedCount = skipped;
-      routerCounters.rippedCount = ripped_item_count;
-      routerCounters.failedToBeRoutedCount = not_routed;
-      routerCounters.routedCount = routed;
-      routerCounters.incompleteCount = new RatsNest(board).incomplete_count();
-
-      this.fireBoardUpdatedEvent(stats, routerCounters, this.board);
-
-      // Shuffle the items to route
-      shuffle(autoroute_item_list, new Random());
-
-      // Let's go through all items to route
-      for (Item curr_item : autoroute_item_list)
-      {
-        // If the user requested to stop the auto-router, we stop it
-        if (this.is_interrupted)
-        {
-          break;
-        }
-
-        // Let's go through all nets of this item
-        for (int i = 0; i < curr_item.net_count(); ++i)
-        {
-          // If the user requested to stop the auto-router, we stop it
-          if (this.thread.is_stop_auto_router_requested())
-          {
-            this.is_interrupted = true;
-            break;
-          }
-
-          // We visually mark the area of the board, which is changed by the auto-router
-          board.start_marking_changed_area();
-
-          // Do the auto-routing step for this item (typically PolylineTrace or Pin)
-          SortedSet<Item> ripped_item_list = new TreeSet<>();
-          //boolean useSlowAlgorithm = this.board.rules.get_use_slow_autoroute_algorithm();
-          boolean useSlowAlgorithm = p_pass_no % 4 == 0;
-          var autorouterResult = autoroute_item(curr_item, curr_item.get_net_no(i), ripped_item_list, p_pass_no, useSlowAlgorithm);
-          if (autorouterResult.state == AutorouteAttemptState.ROUTED)
-          {
-            // The item was successfully routed
-            ++routed;
-          }
-          else if ((autorouterResult.state == AutorouteAttemptState.ALREADY_CONNECTED) || (autorouterResult.state == AutorouteAttemptState.NO_UNCONNECTED_NETS) || (autorouterResult.state == AutorouteAttemptState.CONNECTED_TO_PLANE))
-          {
-            // The item doesn't need to be routed
-            ++skipped;
-          }
-          else
-          {
-            job.logDebug("Autorouter " + autorouterResult.details);
-            ++not_routed;
-          }
-          --items_to_go_count;
-          ripped_item_count += ripped_item_list.size();
-
-          BoardStatistics boardStatistics = board.get_statistics();
-          routerCounters.passCount = p_pass_no;
-          routerCounters.queuedToBeRoutedCount = items_to_go_count;
-          routerCounters.skippedCount = skipped;
-          routerCounters.rippedCount = ripped_item_count;
-          routerCounters.failedToBeRoutedCount = not_routed;
-          routerCounters.routedCount = routed;
-          routerCounters.incompleteCount = new RatsNest(board).incomplete_count();
-          this.fireBoardUpdatedEvent(boardStatistics, routerCounters, this.board);
-        }
-      }
-
-      if (this.remove_unconnected_vias)
-      {
-        remove_tails(Item.StopConnectionOption.NONE);
-      }
-      else
-      {
-        remove_tails(Item.StopConnectionOption.FANOUT_VIA);
-      }
-
-      // We are done with this pass
-      this.air_line = null;
-      return true;
-    } catch (Exception e)
-    {
-      job.logError("Something went wrong during the auto-routing", e);
-      this.air_line = null;
-      return false;
-    }
   }
 
   private void remove_tails(Item.StopConnectionOption p_stop_connection_option)
