4cef8e66ffb6326e14919e5f6a9506a778983887
Andras Fuchs <andras.fuchs@gmail.com>
2025-12-11 16:46:09 +0100

    Add @Override to clone method in DesignRulesCheckerSettings
    Annotated the clone() method with @Override to clarify that it overrides a method from a superclass, improving code clarity and maintainability.

--- src/main/java/app/freerouting/settings/DesignRulesCheckerSettings.java
+++ src/main/java/app/freerouting/settings/DesignRulesCheckerSettings.java
@@ -16,6 +16,7 @@ public class DesignRulesCheckerSettings implements Serializable
   /**
    * Copy constructor
    */
+  @Override
   public DesignRulesCheckerSettings clone()
   {
     DesignRulesCheckerSettings clone = new DesignRulesCheckerSettings();
