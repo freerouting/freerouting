023627239363f911b8d0e3f1e3b1cf4a35f2899d
Andras Fuchs <andras.fuchs@gmail.com>
2026-01-07 21:46:20 +0100

    Update analytics key handling in settings
    Removed the hardcoded Segment write key and added documentation for the BigQuery service account key. The BigQuery key is now set via an environment variable and is used only by the API for analytics.

--- src/main/java/app/freerouting/settings/UsageAndDiagnosticDataSettings.java
+++ src/main/java/app/freerouting/settings/UsageAndDiagnosticDataSettings.java
@@ -8,8 +8,19 @@ public class UsageAndDiagnosticDataSettings implements Serializable {
 
   @SerializedName("disable_analytics")
   public boolean disableAnalytics;
-  @SerializedName("segment_write_key")
-  public transient String segmentWriteKey = "G24pcCv4BmnqwBa8LsdODYRE6k9IAlqR";
+  /*
+   * The bigquery_service_account_key is JSON file that is generated when a new
+   * service account key is created in the Google Cloud Console / IAM & Admin /
+   * Service accounts / Keys.
+   * 
+   * The value of this key is set to the environment variable
+   * FREEROUTING__USAGE_AND_DIAGNOSTIC_DATA__BIGQUERY_SERVICE_ACCOUNT_KEY.
+   * 
+   * This key is used strictly for analytics purposes and only by the API.
+   * 
+   * The desktop and CLI clients do not use this key, they call the API endpoint
+   * for analytics.
+   */
   @SerializedName("bigquery_service_account_key")
   public transient String bigqueryServiceAccountKey;
   @SerializedName("logger_key")
